3d4d8246f2f8da015009ea0cfaeefccfa539446d
JulenBertizOrbegozo <jvertizorb1@educacion.navarra.es>
2025-04-14 17:18:56 +0200

    Proyecto en blanco
    El proyecto esta creado con el main y la clase calculadora

---
+++ PruebasAutomaticas_JulenBertiz/src/test/java/org/example/CalculadoraTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+
+class CalculadoraTest {
+
+    @Test
+    void suma() {
+    }
+
+    @Test
+    void resta() {
+    }
+
+    @Test
+    void multiplicacion() {
+    }
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void esPar() {
+    }
+
+    @Test
+    void maximo() {
+    }
+
+    @Test
+    void esDivisible() {
+    }
+}
