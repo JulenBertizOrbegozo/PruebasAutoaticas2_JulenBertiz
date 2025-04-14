c11264d1e628e593aef3fc68b6cf280aa28d28c3
JulenBertizOrbegozo <jvertizorb1@educacion.navarra.es>
2025-04-14 17:22:21 +0200

    Clase CalculadoraTest creada
    La clase CalculadoraTest esta creada pero todavia no hay codigo.

---
+++ PruebasAutomaticas_JulenBertiz/src/main/java/org/example/CalculadoraTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+
+class CalculadoraTest {
+
+    @Test
+    void suma() {
+    }
+
+    @Test
+    void resta() {
+    }
+
+    @Test
+    void multiplicacion() {
+    }
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void esPar() {
+    }
+
+    @Test
+    void maximo() {
+    }
+
+    @Test
+    void esDivisible() {
+    }
+}
