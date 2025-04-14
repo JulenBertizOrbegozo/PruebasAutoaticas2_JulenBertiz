a53099b59932f5fd761cca1dd26bdce98a457830
JulenBertizOrbegozo <jvertizorb1@educacion.navarra.es>
2025-04-14 17:45:19 +0200

    Metodo multiplicacion
    El metodo multiplicacion esta realizado, compara que 2*4 sea igual a 8.

--- PruebasAutomaticas_JulenBertiz/src/test/java/org/example/CalculadoraTest.java
+++ PruebasAutomaticas_JulenBertiz/src/test/java/org/example/CalculadoraTest.java
@@ -1,20 +1,33 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 class CalculadoraTest {
 
+    Calculadora calc;
+    @BeforeEach
+    void crear(){
+        calc = new Calculadora();
+    }
     @Test
     void suma() {
+        int suma = calc.suma(1,2);
+        assertEquals(3, suma);
     }
 
     @Test
     void resta() {
+        int resta = calc.resta(34,3);
+        assertEquals(31,resta);
     }
 
     @Test
     void multiplicacion() {
+        int multiplicacion = calc.multiplicacion(2,4);
+        assertEquals(8, multiplicacion);
     }
 
     @Test
